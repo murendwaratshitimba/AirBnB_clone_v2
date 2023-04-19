8434792cd5de0f59c82cea7e01c638b262673b76
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-04-20 01:29:22 +0200

    1. Bug free!

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,13 +19,6 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
-
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
@@ -40,4 +33,9 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
-
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
